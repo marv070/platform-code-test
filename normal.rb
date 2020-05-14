0277cb0f5abbf37be0e2d5e273198f10f78306e7
Marvin Laucher <marvinlaucher@Marvins-MacBook-Pro.local>
2020-05-14 12:35:17 -0400

    Account for quality never going below zero in normal award.

--- normal.rb
+++ normal.rb
@@ -2,7 +2,7 @@ class Normal
 	
 	def self.update(award)
 		award.expires_in -= 1
-		award.expires_in > 0 ? award.quality -= 1 : award.quality -= 2
+	    award.quality = [(award.expires_in > 0 ? award.quality - 1  : award.quality - 2),0].max
 		award
 	end
 end

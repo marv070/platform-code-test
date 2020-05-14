6f0a2fc232230fc9e7b3b9199882cdfb476b9e87
Marvin Laucher <marvinlaucher@Marvins-MacBook-Pro.local>
2020-05-14 13:36:57 -0400

    Accounted for quality based on expiration in blue compare.

--- blue_compare.rb
+++ blue_compare.rb
@@ -2,8 +2,8 @@ class BlueCompare
 	
 
 	def self.update(award)
-		award.expires_in -= 1
 		award.quality = calculate_quality(award.expires_in, award.quality)
+		award.expires_in -= 1
 
 		award
 	end
@@ -12,11 +12,14 @@ class BlueCompare
 		case expires_in
 		when 0
 			quality = 0
-		when 1...5
+		when 1..5
+			# binding.pry
 			quality += 3
-		when 6...10
+		when 6..10
+			# binding.pry
 			quality += 2
 		else
+			# binding.pry
 			quality += 1
 		end
 		[quality,50].min

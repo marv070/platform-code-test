e28ae25bdf8c3fd31ac0b5e0922615b48a168436
Marvin Laucher <marvinlaucher@Marvins-MacBook-Pro.local>
2020-05-14 14:11:35 -0400

    Acounted for quality of blue star based on spec.

--- blue_star.rb
+++ blue_star.rb
@@ -1,8 +1,8 @@
 class BlueStar
 	
 	def self.update(award)
+		award.quality = [(award.expires_in < 1 ? award.quality - 4  : award.quality - 2),0].max
 		award.expires_in -= 1
-		award.quality = [award.quality - 2,0].max
 		award
 	end
 end
